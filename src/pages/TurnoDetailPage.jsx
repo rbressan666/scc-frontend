cf05a394e2451b530689ba0e7418125980149231
rbressan <rbressan666@users.noreply.github.com>
2026-04-28 13:31:27 -0300

    contagem v7.2

--- src/pages/TurnoDetailPage.jsx
+++ src/pages/TurnoDetailPage.jsx
@@ -5,7 +5,7 @@ import { Button } from '../components/ui/button';
 import { Badge } from '../components/ui/badge';
 import { ArrowLeft, Plus, Save, Check } from 'lucide-react';
 import { turnosService } from '../services/api';
-import { useToast } from '../components/ui/use-toast';
+import { useToast } from '../hooks/use-toast';
 
 const TurnoDetailPage = () => {
   const { id } = useParams();
